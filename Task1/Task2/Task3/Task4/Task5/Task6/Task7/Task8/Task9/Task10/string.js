c9dcb5488cb50efa6f8b77b5669fdcee790f649c
shailendra <rajakshailendra123@gmail.com>
2022-07-02 00:34:57 +0530

    ~/Desktop/Javascript stringpushed.

---
+++ Task1/Task2/Task3/Task4/Task5/Task6/Task7/Task8/Task9/Task10/string.js
@@ -0,0 +1,15 @@
+// charAt(c): It returns the character at the “c” position within the string.
+//var mystring='javascript!!!';
+//console.log(mystring.charAt(7));.
+//concat(p1, p2): Combines one or more strings and returns the concatenated string. Remember that the original string is not modified.
+// var str1="javascript"
+// var str2=str1.concat("  is ","Awesome")
+// console.log(str2);
+//indexOf(substr, [start_from]): This method Searches and returns the index number of the searched character within the string. If not found, it will return -1 . “Start_from” is an optional argument specifying the position within string to begin the search. Default is 0.
+//indexOf(char/substring)
+//var str1="hi my name is Shailendra";
+//var str2=str1.indexOf("hi");
+//console.log(str2);
+//lastIndexOf(substr, [start_from]): This method searches and  returns the index number of the searched character within the string. This method will return the index of the last occurrence of a specified text in a string.If the character or string is not found, -1 is returned. "Start_from" is an optional argument specifying the position within string to begin the search. Default is string.length-1
+var myString = 'javascript Node.js';
+console.log(myString.lastIndexOf('N'));
